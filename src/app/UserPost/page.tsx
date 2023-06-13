79b19656423222e1367556c07d689d39f1eb8708
garriagus <agustin_g08@hotmail.com>
2023-06-13 02:33:16 -0300

    a

--- src/app/UserPost/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserPostPage = () => {
-  return <div>src/app/UserPost</div>;
-};
-
-export default UserPostPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-export default function Home() {
-  return <div className="flex flex-wrap gap-5">
-    esto es un src/app/pagess
-  </div>;
+export default async function Home() {
+  return (
+    <>
+      <section className="bg-ct-blue-600 min-h-screen pt-20">
+        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
+          <p className="text-3xl font-semibold">
+            Welcome to NextAuth with Prisma ORM and Next.js 13
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
