cc1528701c23e60f359d3d9f6a13e0bae3b095d4
garriagus <agustin_g08@hotmail.com>
2023-06-13 21:22:23 -0300

    starting point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,31 @@ import Image from 'next/image'
 import BlogPostContainer from "@/components/BlogPostContainer";
 import { getAllPosts } from "@/lib/getData";
 import { Metadata } from "next";
+import { prisma } from "@/lib/prisma";
 
 export default async function Home() {
   const posts = await getAllPosts();
-
+  let users = await prisma.user.findMany();
   return (
     <main>
       <div className="flex justify-center">
         <h1 className="m-2 text-center text-2xl font-bold capitalize bg-gradient-to-r from-red-600 to-purple-500 inline bg-clip-text text-transparent">
-          Wellcome to dummy blog
+          Wellcome to dummy blog          
         </h1>
-      </div>
+      </div>    
+      {users.map((user) => (
+          <div
+            key={user.id}
+            style={{ border: "1px solid #ccc", textAlign: "center" }}
+          >
+            <img
+              src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+              alt='asd'
+              style={{ height: 180, width: 180 }}
+            />
+            <h3>{user.name}</h3>
+          </div>
+        ))}
       <BlogPostContainer blogPosts={posts} />
     </main>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-import { prisma } from "@/lib/prisma";
 import { compare } from "bcryptjs";
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
@@ -7,25 +6,56 @@ import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 // import Twitch from "../../../providers/twitch";
 import Auth0Provider from "next-auth/providers/auth0";
-import AzureADProvider from "next-auth/providers/azure-ad";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient();
+
+
 export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
+  adapter: PrismaAdapter(prisma),
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+      // ** ...add more providers here
+      
+  ],  
+  secret: process.env.NEXTAUTH_SECRET,  
+    // ** Please refer to https://next-auth.js.org/configuration/options#session for more `session` options
+    session: {
+      /*
+       * Choose how you want to save the user session.
+       * The default is `jwt`, an encrypted JWT (JWE) stored in the session cookie.
+       * If you use an `adapter` however, NextAuth default it to `database` instead.
+       * You can still force a JWT session by explicitly defining `jwt`.
+       * When using `database`, the session cookie will only contain a `sessionToken` value,
+       * which is used to look up the session in the database.
+       */
+      strategy: 'jwt',
   
-  ],
-  callbacks: {
-    async jwt({ token }) {
-      token.role = "member";
-      return token;
+      // ** Seconds - How long until an idle session expires and is no longer valid
+      maxAge: 30 * 24 * 60 * 60 // ** 30 days
     },
+    callbacks: {
+      /*
+       * While using `jwt` as a strategy, `jwt()` callback will be called before
+       * the `session()` callback. So we have to add custom parameters in `token`
+       * via `jwt()` callback to make them accessible in the `session()` callback
+       */
+      async jwt({ token, user }) {
+        if (user) {
+          /*
+           * For adding custom parameters to user in session, we first need to add those parameters
+           * in token which then will be available in the `session()` callback
+           */
+          token.role = "member";
+          return token;
+        }
+  
+        return token
+      },  
   },
 };
