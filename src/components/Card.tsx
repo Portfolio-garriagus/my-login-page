62a19d53c6632718bdc94707d3cfd32443ff77f9
agustin_g08@hotmail.com <garriagus>
2023-06-15 16:03:25 -0300

    init

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,11 +6,12 @@ import React from "react";
 interface Props {
   blogPost: Post;
 }
+
 const Card = ({ blogPost }: Props) => {
   return (
     <div className=" mb-6 last:mb-0 lg:mb-0 flex flex-col gap-2 group  justify-center items-center  ">
       <Image
-        src={blogPost.imageUrl}
+        src={`${blogPost.imageUrl}.jpg`}
         alt={blogPost.title}
         className={"rounded-md drop-shadow transition group-hover:scale-105"}
         width={400}
@@ -19,8 +20,7 @@ const Card = ({ blogPost }: Props) => {
       <h6 className="text-lg text-slate-800">{blogPost.title}</h6>
       <p className="text-slate-500 text-center">{blogPost.authorId}</p>
       <Link href={`/post/${blogPost.id}`} className={"text-slate-500 hover:text-sky-500 flex"}>
-        <span>See More</span>
-        
+        <span>See More</span>        
       </Link>
     </div>
   );
