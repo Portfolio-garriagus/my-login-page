9c25a7fe8d2b2541b61647fdffafb4ae709a468a
agustin_g08@hotmail.com <garriagus>
2023-06-22 14:48:00 -0300

    dark mode

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,12 +2,19 @@
 
 import React, { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+
 interface Props {
   children: ReactNode;
 }
 
-const Providers = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <NextThemesProvider {...props}>
+      <SessionProvider>{children}</SessionProvider>
+    </NextThemesProvider>
+  );
 };
 
 export default Providers;
